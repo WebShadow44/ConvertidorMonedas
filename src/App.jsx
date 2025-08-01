1ebb022b1d17f67c9e2d16e3a70e483c449f27f0
Alejandro LB <alelb0803@gmail.com>
2025-08-01 14:47:33 -0500

    Convertidor de monedas

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ConverterForm from "./components/ConverterForm";
+const App = () => {
+  return (
+    <div className="currency-converter">
+      <h2 className="converter-title">Convertidor de Cambio de Monedas</h2>
+      <ConverterForm />
+    </div>
+  )
+}
+export default App;
